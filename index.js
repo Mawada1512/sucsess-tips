6eccca2ec0c8c9c7de01d56d6362e8bf570cd852
Mawada1512811 <mawadabeedy5@gmail.com>
2025-12-25 20:32:00 +0200

    add apis

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function getTips(event) {
-  event.preventDefault();
+function genaretTips(response) {
   new Typewriter("#tip", {
-    strings: "tips will apper here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function getTips(event) {
+  event.preventDefault();
+  let usersInput = document.querySelector("#users-input");
+  let key = "6fftdd01472a035eo394f08abdd05bea";
+  let context =
+    "you are a life coach and personal trainer who is very export and you have red a lot of books in general you have experience in life and you can give advices and tips in any topic to help people, please write the tips and advices in very short way , be specific and  fallow what user want them to be about, at the end make <br/> and sign it with with love MIMI, put the sign in <strong> ";
+  let prompt = ` User Instructions: Generate a Tip about ${usersInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+  axios.get(apiUrl).then(genaretTips);
 }
 
 let tipForm = document.querySelector("#tip-form");
